b5e1ffdfab1eb84186e5f5fb91424d658aa91ff1
bayu <bayuferdiman1@gmail.com>
2021-07-18 13:39:25 +0700

    day 2-july 18

---
+++ 02_Day_Variables_builtin_functions/exercise.py
@@ -0,0 +1,67 @@
+# Exercise level 2
+first_name = 'Bayu'
+last_name = 'Ferdiman'
+
+# 2
+# print(len(first_name)) # 4
+
+# 3
+# print(len(first_name) , len(last_name)) # 4
+
+# 4
+num_one = 5
+num_two = 4
+# i
+variable_total = num_one + num_two
+# print(variable_total) # 9
+
+#ii
+variable_diff = num_one - num_two
+# print(variable_diff)
+
+#iii
+variable_product = num_one * num_two
+# print(variable_product)
+
+#iv
+variable_division = num_one / num_two
+# print(variable_division)
+
+#v
+variable_remainder = num_one % num_two
+# print(variable_remainder)
+
+#vi
+variable_exp = num_one ** num_two
+# print(variable_exp)
+
+#vii
+variable_floor_division = num_one // num_two
+# print(variable_floor_division)
+
+
+# 5 
+#i
+r = input('Input radius of a circle ?')
+phi = 3.14
+area_of_circle = phi * (r**2)
+# print(area_of_circle)
+
+#ii
+circum_of_circle = 2 * phi * r
+# print(circum_of_circle)
+
+#iii
+r = input('Input radius of a circle ?')
+
+#6
+first_name = input('What is your name: ')
+last_name = input('What is your last name: ')
+age = input('How old are you? ')
+country = input('Your country ?')
+
+# print(first_name,last_name,age,country)
+
+#7 
+#run help keywords in python shell
+
--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -17,24 +17,31 @@ person_info = {
 
 # Printing the values stored in the variables
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+# print('First name:', first_name)
+# print('First name length:', len(first_name))
+# print('Last name: ', last_name)
+# print('Last name length: ', len(last_name))
+# print('Country: ', country)
+# print('City: ', city)
+# print('Age: ', age)
+# print('Married: ', is_married)
+# print('Skills: ', skills)
+# print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
 
 first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+# print(first_name, last_name, country, age, is_married)
+# print('First name:', first_name)
+# print('Last name: ', last_name)
+# print('Country: ', country)
+# print('Age: ', age)
+# print('Married: ', is_married)
+
+
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
