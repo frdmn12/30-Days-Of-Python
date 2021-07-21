9b77938cc91366e5845e6117e8da1feae7fc2d8c
bayu <bayuferdiman1@gmail.com>
2021-07-21 09:26:12 +0700

    day 3 stuck

---
+++ 03_Day_Operators/exercise.py
@@ -0,0 +1,60 @@
+# Exercise
+# 1
+age = 19
+# 2
+height = 5.7
+# 3
+complex_num = 9 + 1j
+
+# 4
+# base = eval(input('Base ?'))
+# height = eval(input('Height ?'))
+# area_of_triangle = 0.5 * base * height
+# print('are of triangle', area_of_triangle)
+
+# 5
+# side_a = eval(input("side a ? "))
+# side_b = eval(input("side b ? "))
+# side_c = eval(input("side c ? "))
+
+# perimeter = side_a + side_b + side_c
+# print('parimeter = ', perimeter)
+
+# 6
+# length = eval(input("length ?"))
+# width = eval(input("width ? "))
+# area = length * width
+# perimeter = 2 * area
+# print("parimeter ", perimeter)
+
+#7
+# pi = 3.14
+# radius = eval(input("radius ? "))
+# area = pi * r **r
+# cicumfrance = pi * 2 * r 
+# print("area", area)
+# print("circumfrance", cicumfrance)
+
+#8
+# slope1 = 2  
+
+
+#9 
+# x1 = 2
+# y1 = 2
+# x2 = 6
+# y2 = 10
+# slope2 = (y2 - y1) / (x2 - x1)
+# print(slope2)
+
+#10
+# print(slope1 == slope2)
+# print(slope1 > slope2)
+# print(slope1 >= slope2)
+# print(slope1 <= slope2)
+# print(slope1 < slope2)
+# print(slope1 > slope2)
+
+
+#11
+y = x**2 + 6 + 9
